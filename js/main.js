ed3fe6ced9fec4f65a06121419a4e81f95a21ecf
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-05-08 15:33:35 -0300

    criando botao temporario para trocar a pagina

--- js/main.js
+++ js/main.js
@@ -12,4 +12,15 @@
 	};
 	fullHeight();
 
+	$(".toggle-password").click(function() {
+
+	  $(this).toggleClass("fa-eye fa-eye-slash");
+	  var input = $($(this).attr("toggle"));
+	  if (input.attr("type") == "password") {
+	    input.attr("type", "text");
+	  } else {
+	    input.attr("type", "password");
+	  }
+	});
+
 })(jQuery);
